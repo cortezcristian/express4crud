7f64de2b1ceb48db089b34fe1a1b6c93dcfe11cc
Cristian Cortez <cortez.cristian@gmail.com>
2014-11-09 00:19:29 -0300

    Adding models, test, main route file

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/crudtest');
 
-var app = express();
+var app = exports.app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+require('./routes/main.js');
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
---
+++ routes/main.js
@@ -0,0 +1,5 @@
+var app = module.parent.exports.app;
+
+app.get('/list', function(req, res){
+    res.end('It works!');
+});
