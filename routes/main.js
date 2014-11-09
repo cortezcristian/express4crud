cc9726b96ed847152b38725c1bbfaf46376793f2
Cristian Cortez <cortez.cristian@gmail.com>
2014-11-09 00:38:17 -0300

    Hook models to routes

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,8 @@
 var app = module.parent.exports.app;
+var Persons = require('../models/persons.js');
 
 app.get('/list', function(req, res){
-    res.end('It works!');
+    Persons.find({}, function(err, docs){
+        res.json(docs);
+    });
 });
