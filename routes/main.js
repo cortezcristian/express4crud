410675b6ecc671cee6e5bec4cb4cd7a97a0c5cf3
Cristian Cortez <cortez.cristian@gmail.com>
2014-11-10 12:00:15 -0300

    Adding new view

--- routes/main.js
+++ routes/main.js
@@ -6,3 +6,11 @@ app.get('/list', function(req, res){
         res.render('list', { title: 'List', persons: docs});
     });
 });
+
+app.get('/p/new', function(req, res){
+    res.render('new', { title: 'New'});
+});
+
+app.post('/p/new', function(req, res){
+    res.render('new', { title: 'New'});
+});
